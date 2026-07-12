92f56a732564f1e937bc6693ae5a9fcf5174c9ac
sazzad tomal <sazzadtomal1@gmail.com>
2026-07-12 12:29:04 +0600

    Remove unused commented code and update address

--- app/components/home/ContactSection.js
+++ app/components/home/ContactSection.js
@@ -1,60 +1,3 @@
-// import React from "react";
-
-// export default function ContactSection() {
-//   return (
-//     <main>
-//       <div>
-//         <div className="rts-contact-area contact-one">
-//           <div className="container">
-//             <div className="row align-items-center g-0">
-//               <div className="col-lg-4 col-md-12 col-sm-12 col-12">
-//                 <div className="contact-image-one">
-//                   <img src="/assets/images/contact/01.jpg" alt="" />
-//                 </div>
-//               </div>
-//               <div className="col-lg-8 col-md-12 col-sm-12 col-12">
-//                 <div className="contact-form-area-one">
-//                   <div className="rts-title-area contact text-start">
-//                     <p className="pre-title">Make An Appointment</p>
-//                     <h2 className="title">Request a free quote</h2>
-//                   </div>
-//                   <div id="form-messages"></div>
-//                   <form
-//                     id="contact-form"
-//                     action="https://finbiz-nextjs.netlify.app/mailer.php"
-//                     method="post"
-//                   >
-//                     <div className="name-email">
-//                       <input type="text" placeholder="Your Name" name="name" />
-//                       <input
-//                         type="email"
-//                         placeholder="Email Address"
-//                         name="email"
-//                       />
-//                     </div>
-//                     <input
-//                       type="text"
-//                       placeholder="Business Topic"
-//                       name="subject"
-//                     />
-//                     <textarea
-//                       placeholder="Type Your Message"
-//                       name="message"
-//                     ></textarea>
-//                     <button type="submit" className="rts-btn btn-primary">
-//                       Submit Message
-//                     </button>
-//                   </form>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
 import styles from "./ContactSection.module.css";
 
 const contactDetails = [
@@ -73,7 +16,7 @@ const contactDetails = [
   {
     icon: "fas fa-map-marker-alt",
     title: "Visit / Postal Address",
-    text: "40 Winsford Terrace, London 3SD 1BS",
+    text: "40 Winsford Terrace, Great Cambridge Road, London N18 1BS",
     href: "#map",
   },
 ];
--- app/components/home/MapSection.js
+++ app/components/home/MapSection.js
@@ -1,63 +1,6 @@
-// import React from "react";
-
-// export default function MapSection() {
-//   return (
-//     <main>
-//       {" "}
-//       <div>
-//         <div className="rts-map-area bg-light-white" id="address">
-//           <div className="container-fluid">
-//             <div className="row align-items-center">
-//               <div className="col-lg-6">
-//                 <div className="mapdetails-inner-one">
-//                   <div className="left-area single-wized">
-//                     <h5 className="title">Get in touch</h5>
-//                     <div className="details">
-//                       <p>Work and general inquiries</p>
-//                       <a className="number" href="#">
-//                         +3509.120-8605
-//                       </a>
-//                       <p className="time-header">Assistance hours:</p>
-//                       <p className="time">
-//                         Monday – Friday <br /> 6 am to 8 pm EST
-//                       </p>
-//                     </div>
-//                   </div>
-//                   <div className="right-area single-wized">
-//                     <h5 className="title">Post Address</h5>
-//                     <div className="details">
-//                       <p>Service Office</p>
-//                       <a href="#">
-//                         786 Dortans Ave, Otam Sites, <br />
-//                         CA 36108, United States
-//                       </a>
-//                       <p className="headoffice">Head Office</p>
-//                       <p className="office">142 Drive Lane. USA</p>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//               <div className="col-lg-6">
-//                 <iframe
-//                   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14602.288851207937!2d90.47855065!3d23.798243149999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1663151706353!5m2!1sen!2sbd"
-//                   width="600"
-//                   height="450"
-//                   style={{ border: "0" }}
-//                   loading="lazy"
-//                   referrerPolicy="no-referrer-when-downgrade"
-//                 ></iframe>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
 import styles from "./MapSection.module.css";
 
-const address = "40 Winsford Terrace, London 3SD 1BS";
+const address = "40 Winsford Terrace, Great Cambridge Road, London N18 1BS";
 const mapSrc = `https://www.google.com/maps?q=${encodeURIComponent(
   address,
 )}&output=embed`;
